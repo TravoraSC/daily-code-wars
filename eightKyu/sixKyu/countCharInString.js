d051baa3aa61cb7fa08ea6fb6cb318f64e3e8a2f
Travora Senecharles <travora9@gmail.com>
2022-08-26 18:19:17 -0400

    a bit of OOP

---
+++ eightKyu/sixKyu/countCharInString.js
@@ -0,0 +1,25 @@
+// In this Kata, we are going to determine if the count of each of the characters in a string can be equal if we remove a single character from that string.
+
+// For example:
+
+// solve('abba') = false -- if we remove any character, the count of each character will not be equal.
+// solve('abbba') = true -- if we remove one b, the count of each character becomes 2.
+// solve('aaaa') = true -- if we remove one character, the remaining characters have same count.
+// solve('wwwf') = true -- if we remove f, the remaining letters have same count.
+// More examples in the test cases. Empty string is not tested.
+
+// Good luck!
+
+function count (string) {  
+  let strCountObj = {}
+  for(let i = 0 ; i < string.length ; i++) {
+    if(strCountObj.hasOwnProperty(string[i])) {
+      strCountObj[string[i]]++
+    }else{
+      strCountObj[string[i]] = 1
+    }
+  }
+   return strCountObj
+}
+
+
